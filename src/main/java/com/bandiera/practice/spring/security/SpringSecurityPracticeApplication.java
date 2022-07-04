65db7004f41ae4b8fa7080e29b0f39e5987b0d73
bandiera <bandiera81@gmail.com>
2022-07-04 21:45:33 +0900

    Add #2 프로젝트 세팅

---
+++ src/main/java/com/bandiera/practice/spring/security/SpringSecurityPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.bandiera.practice.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Security Practice Application
+ */
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+}
